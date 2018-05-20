2ab04e807623fe906ee6d3d190b1ea002ea0ebe5
Aditya Giri <dtrg21@gmail.com>
2018-05-20 20:42:02 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return <div>Would You Rather starting point.</div>
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style/normalize.css'
+import './style/index.css'
+import App from './components/App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
