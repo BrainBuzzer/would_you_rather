035ff0acc3a10a0f6df335aed812332ce3666eef
Aditya Giri <dtrg21@gmail.com>
2018-05-22 13:30:12 +0530

    [fix] App Router problem

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import { Route, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Home from './Home'
 import Auth from './Auth'
@@ -21,4 +21,4 @@ class App extends Component {
   }
 }
 
-export default connect()(App)
+export default withRouter(connect()(App))
