064bf09fcc12cd0ca02804349991f2e1b8bb8a3e
Aditya Giri <dtrg21@gmail.com>
2018-05-22 15:29:57 +0530

    [store] Add user registration

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,7 +1,7 @@
 import { getUsers } from './users'
 import { getQuestions } from './questions'
-import { getInitialData } from '../utils/api'
 import { handleUserLogin } from './auth'
+import { getInitialData, saveNewUser } from '../utils/api'
 
 export const GET_INITIAL_DATA = 'GET_INITIAL_DATA'
 
@@ -12,5 +12,19 @@ export function handleInitialData () {
       dispatch(getQuestions(questions))
       dispatch(handleUserLogin(null))
     })
+  }
+}
+
+export function handleUserRegistration (username, name) {
+  return (dispatch) => {
+    saveNewUser({ username, name })
+      .then((users) => {
+        if (users.error) {
+          console.error('Username Already taken')
+        } else {
+          dispatch(handleUserLogin(username))
+          dispatch(getUsers(users))
+        }
+      })
   }
 }
