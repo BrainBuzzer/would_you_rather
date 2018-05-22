fc93bc4962dd45f5c9d24d4618495608c7fa30c8
Aditya Giri <dtrg21@gmail.com>
2018-05-22 13:27:08 +0530

    [dev] Add redux devtools

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './style/normalize.css'
 import './style/index.css'
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import reducer from './reducers'
 import middleware from './middleware'
 import { Provider } from 'react-redux'
 
-const store = createStore(reducer, middleware)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhancers(middleware))
 
 ReactDOM.render(
   <Provider store={store}>
