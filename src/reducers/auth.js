17a27009988a56c6e743ef482396456b216489da
Aditya Giri <dtrg21@gmail.com>
2018-05-23 15:53:43 +0530

    [store] Add logout action

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,9 +1,14 @@
-import { USER_LOGIN } from '../actions/auth'
+import {
+  USER_LOGIN,
+  USER_LOGOUT
+} from '../actions/auth'
 
 export function auth (state = null, action) {
   switch (action.type) {
     case USER_LOGIN:
       return action.id
+    case USER_LOGOUT:
+      return null
     default:
       return state
   }
